9f5ad4578268e3ec78c57b921cf99a439e373f8f
Khadidja <khadija.aitsiali@gmail.com>
2024-07-07 00:06:16 +0200

    files prepared

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Work from "./components/Work";
+import Footer from "./components/Footer";
+import NotFound from "./components/NotFound";
 
-function App() {
+const App = () => {
   return (
+    <BrowserRouter>
+      <Header/>
+      <Routes>
 
-    <div>
-      <h1>deploy for test</h1>
-    </div>
-    
+        <Route path="/" element={<Home />}/>
+        <Route path="/work" element={<Work />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path="*" element={<NotFound />}/>
+
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+import './../styles/header.css';
+
+const Header = () => {
+  return (
+    <nav>
+
+      <NavLink>
+
+
+
+      </NavLink>
+      
+    </nav>
+  );
+};
+
+export default Header;
