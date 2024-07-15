989f6c6aaa16466436204d77682b8600a6e398e1
Khadidja <khadija.aitsiali@gmail.com>
2024-07-16 00:29:24 +0200

    work on contact part

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,42 @@
 import React from 'react';
 import './../styles/contact.css';
+import github from './../shared/social/github.svg';
+import gmail from './../shared/social/gmail.svg';
+import linkedin from './../shared/social/linkedin.svg';
+import phone from './../shared/social/telephone.svg';
 
 const Contact = () => {
 
   return (
     <section className='contact_container'>
 
-      <div className="contact_grid">
+      <div className='contact_title'>
+        <h4>get in touch</h4>
+      </div>
 
-        <div className="left">
-          <h4>have a project <br /> in mind ?</h4>
-        </div>
-        <div className="center">
-          <h4>yo</h4>
-        </div>
-        <div className="right">
-          <h4>yo</h4>
-        </div>
-
+      <div className="social_list">
+        <ul>
+          <li>
+            <a href="tel:+336.14.72.05.66" rel="noopener noreferrer" target="_blank" title="Call me">
+              <img src={phone} alt="Phone icon" />
+            </a>
+          </li>
+          <li>
+            <a href="mailto:khadidja.aitsiali@gmail.com" rel="noopener noreferrer" target="_blank" title="Email me">
+              <img src={gmail} alt="Gmail logo" />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/khadidja-ait-si-ali/" rel="noopener noreferrer" target="_blank" title="Linkedin me">
+              <img src={linkedin} alt="Linkedin logo" />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/Khadija-Asa" rel="noopener noreferrer" target="_blank" title="Github me">
+              <img src={github} alt="Github logo" />
+            </a>
+          </li>
+        </ul>
       </div>
 
     </section>
