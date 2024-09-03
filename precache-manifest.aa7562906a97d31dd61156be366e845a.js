403c46d4c905ccbf0a2399f229014c798c8fa85d
Khadidja <khadija.aitsiali@gmail.com>
2024-09-04 00:31:55 +0200

    Updates

---
+++ precache-manifest.aa7562906a97d31dd61156be366e845a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "343f3d97aa2bdfb988d412ee0ccb9d63",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "18f42d088b96912a6d5c",
+    "url": "/react_portfolio/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "26f14d6141c90c2dc6f2",
+    "url": "/react_portfolio/static/css/main.e65c9977.chunk.css"
+  },
+  {
+    "revision": "18f42d088b96912a6d5c",
+    "url": "/react_portfolio/static/js/2.789741e9.chunk.js"
+  },
+  {
+    "revision": "e71a370bc648f150621dbb9292d12619",
+    "url": "/react_portfolio/static/js/2.789741e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26f14d6141c90c2dc6f2",
+    "url": "/react_portfolio/static/js/main.1b9e26a6.chunk.js"
+  },
+  {
+    "revision": "90c2787940c32f4f4098",
+    "url": "/react_portfolio/static/js/runtime-main.e0da2318.js"
+  },
+  {
+    "revision": "212aa815d9a5330fc15854bf0d9a1e99",
+    "url": "/react_portfolio/static/media/Meditative.212aa815.ttf"
+  },
+  {
+    "revision": "edcd89a4b4738c115db680439516643a",
+    "url": "/react_portfolio/static/media/Pergola-Demo.edcd89a4.otf"
+  },
+  {
+    "revision": "5a014cbe3c623db625e2a378539b3470",
+    "url": "/react_portfolio/static/media/circle_contact.5a014cbe.gif"
+  },
+  {
+    "revision": "94c37e2ed19d0ec6b96e8fffcaa86954",
+    "url": "/react_portfolio/static/media/eid_lab.94c37e2e.webp"
+  },
+  {
+    "revision": "8363897c6d02c821bf3458a66e5d5019",
+    "url": "/react_portfolio/static/media/github.8363897c.svg"
+  },
+  {
+    "revision": "b9d61e4d9c6c7aa89cf676bed695ce1e",
+    "url": "/react_portfolio/static/media/khadidja_mockup.b9d61e4d.svg"
+  },
+  {
+    "revision": "86eb5ba6a4c35b1b9bd09c7accc5cccb",
+    "url": "/react_portfolio/static/media/levyna_mockup.86eb5ba6.svg"
+  },
+  {
+    "revision": "c35a4437af99e21b70017b57ca30dd7a",
+    "url": "/react_portfolio/static/media/linkedin.c35a4437.svg"
+  },
+  {
+    "revision": "17d27ffbadd3c89107639499c9770516",
+    "url": "/react_portfolio/static/media/logo.17d27ffb.svg"
+  },
+  {
+    "revision": "c5111dd0354fccd37ddb4fc8cfca1fb0",
+    "url": "/react_portfolio/static/media/moon_mockup.c5111dd0.svg"
+  },
+  {
+    "revision": "6f9ee3136bc869ae48d342bbeb6316d0",
+    "url": "/react_portfolio/static/media/nft_mockup.6f9ee313.svg"
+  },
+  {
+    "revision": "1c35ced044de772caded686cf7778393",
+    "url": "/react_portfolio/static/media/numeriquelles_mockup.1c35ced0.svg"
+  },
+  {
+    "revision": "5eda4009e6c0a4b3bb0b5b05bb9d7908",
+    "url": "/react_portfolio/static/media/space_mockup.5eda4009.svg"
+  },
+  {
+    "revision": "7473c81f5726effa88cbbd31e054fc95",
+    "url": "/react_portfolio/static/media/wired-arrow.7473c81f.gif"
+  }
+]);
